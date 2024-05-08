add976275b1dc250f04bd25912a96b6061bee8e4
Vominhtan20 <votan.224720@gmail.com>
2024-05-08 21:20:35 +0700

    them object

---
+++ Duanmau/Assets/Scenes/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // day la cong viec cua nhom nha
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
